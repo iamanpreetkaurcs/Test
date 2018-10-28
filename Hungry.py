15e9f44dac5fd27bd947a00602587c4f4dea7f1c
Aman <iamanpreetkaurcs@gmail.com>
2018-10-28 12:12:37 -0600

    Second Commit

---
+++ Hungry.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+hungry=input("are you hungry")
+if (hungry == "yes"):
+    print("Eat Samosa")
+else:
+    print("Do your HW")
